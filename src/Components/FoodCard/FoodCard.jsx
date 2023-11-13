323a9b548460c09c0c336340f8845193eeafa392
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-11-13 23:27:45 +0600

    add cart functionaliy added

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,20 +1,59 @@
+import Swal from "sweetalert2";
+import useAuth from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
 
-const FoodCard = ({item}) => {
-    const {image,price,recipe,name}=item;
 
-    return (
-<div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src={image} alt="Shoes" /></figure>
-  <p className="bg-slate-900 absolute right-0 mr-4 mt-4"> ${price} </p>
-  <div className="card-body flex justify-center">
-    <h2 className="card-title">{name}</h2>
-    <p>{recipe}</p>
-    <div className="card-actions justify-center">
-      <button className="btn btn-primary bg-slate-100 border-0 border-b-4 border-orange-400 ">Add To Cart</button>
+const FoodCard = ({ item }) => {
+  const {user}= useAuth();
+  const { image, price, recipe, name ,_id} = item;
+  const navigate= useNavigate()
+  const location= useLocation()
+
+  const handleAddToCart=food=>{
+     if (user && user.email) {
+      // <h3></h3>
+      const cartItem={
+     menuId:_id,
+     email:user.email,
+     name
+      }
+     }
+     else{
+      Swal.fire({
+        title: "You are not logged in",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Please, Login"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // send user to the login
+         navigate('/login' ,{state:{from:location}} )
+        }
+      });
+     }
+
+  }
+
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img src={image} alt="Shoes" />
+      </figure>
+      <p className="bg-slate-900 absolute right-0 mr-4 mt-4"> ${price} </p>
+      <div className="card-body flex justify-center">
+        <h2 className="card-title">{name}</h2>
+        <p>{recipe}</p>
+        <div className="card-actions justify-center">
+          <button onClick={()=>handleAddToCart(item) } className="btn btn-primary bg-slate-100 border-0 border-b-4 border-orange-400 ">
+            Add To Cart
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default FoodCard;
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,61 +1,99 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../Providers/AuthProvider";
-
+import {FaShoppingCart} from 'react-icons/fa'
 const Navbar = () => {
-  const {user,logOut}= useContext(AuthContext)
-  const handleLogout=()=>{
+  const { user, logOut } = useContext(AuthContext);
+  const handleLogout = () => {
     logOut()
-    .then(()=>{})
-    .catch(err=>console.log(err))
-  }
-    const navOptions= <>
-           <li>  <Link to={'/'}>Home</Link> </li>
-        <li>  <Link to={'/menue'}>Our Menue</Link> </li>
+      .then(() => {})
+      .catch((err) => console.log(err));
+  };
+  const navOptions = (
+    <>
+      <li>
+        {" "}
+        <Link to={"/"}>Home</Link>{" "}
+      </li>
+      <li>
+        {" "}
+        <Link to={"/menue"}>Our Menue</Link>{" "}
+      </li>
 
-        <li>  <Link to={'/order/salad'}>Order Food</Link> </li>
+      <li>
+        {" "}
+        <Link to={"/order/salad"}>Order Food</Link>{" "}
+      </li>
+   <li>  
+    <Link>
+
+<button className="btn">
+ <FaShoppingCart className="mr-4"></FaShoppingCart>
+  <div className="badge badge-secondary">+0</div>
+</button>
     
-        {
-          user ? <> 
-          <span> {user?.displayName} </span>
-          <button onClick={handleLogout} className="btn btn-ghost">LogOut</button>
-          </>: <>
-         <li> <Link to={'/login'}>Login</Link> </li>
-          
-          </>
-        }
+    </Link>
+   </li>
 
-        </>
-        
-    return (
+
+
+      {user ? (
         <>
-        <div className="navbar fixed bg-opacity-30  z-10 max-w-screen-xl mx-auto bg-black text-white">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-       {
-        navOptions
-       }
-      </ul>
-    </div>
-    <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-      {
-        navOptions
-      }
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <a className="btn">Button</a>
-  </div>
-</div> 
+          {/* <span> {user?.displayName} </span> */}
+          <button onClick={handleLogout} className="btn btn-ghost">
+            LogOut
+          </button>
         </>
-    );
+      ) : (
+        <>
+          <li>
+            {" "}
+            <Link to={"/login"}>Login</Link>{" "}
+          </li>
+        </>
+      )}
+    </>
+  );
+
+  return (
+    <>
+      <div className="navbar fixed bg-opacity-30  z-10 max-w-screen-xl mx-auto bg-black text-white">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navOptions}
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-xl">Bistro Boss</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navOptions}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
