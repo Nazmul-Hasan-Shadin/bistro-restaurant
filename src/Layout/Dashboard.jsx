87ebe74891aa078e6a7dfd8d53c80c0a24b6a464
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2023-11-16 19:05:33 +0600

    admin dashboard authentication has added

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { FaAd, FaBook, FaCalendar, FaEnvelope, FaHome, FaList, FaShoppingCart, FaUsers, FaUtensils, FaVoicemail } from 'react-icons/fa';
 import { NavLink, Outlet } from 'react-router-dom';
 import useCart from '../hooks/useCart';
+import useAdmin from '../hooks/useAdmin';
 
 const Dashboard = () => {
     const [cart]=useCart();
     // TODO: get isAdmin value from the database
-    const isAdmin=true;
+    const [isAdmin]=useAdmin();
 
     return (
         <div className='flex '>
--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -1,6 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import useAuth from './useAuth';
+import useAxios from './useAxios';
 
 const useAdmin = () => {
+    const {user}= useAuth()
+    const axiosSecure=useAxios()
+ 
+ const {data:isAdmin,isPending:isAdminLoading}=useQuery({
+    queryKey:[user?.email,'isAdmin'],
+    queryFn: async()=>{
+      const res= await axiosSecure.get(`/users/admin/${user.email}`);
+      console.log(res.data.admin);
+       return res.data.admin
+    }
+ })
+ return [isAdmin,isAdminLoading]
 
 };
 
